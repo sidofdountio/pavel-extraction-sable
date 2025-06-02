01c75e34d7e9a62058f573d69ce79a1addf902f9
sidof <sidofdountio406@gmail.com>
2025-06-02 16:23:32 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { FooterAppComponent } from "./footer-app/footer-app.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterAppComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { PricingComponent } from './web-site/pricing/pricing.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './web-site/welcome/welcome.component';
+import { AboutComponent } from './web-site/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: "home",
+        component: WelcomeComponent
+    },
+    {
+        path: "",
+        pathMatch: "full",
+        redirectTo: "home"
+    },
+    {
+        path: "pricing",
+        component: PricingComponent,
+        title: 'Pricing'
+    },
+    {
+        path: "about",
+        component: AboutComponent,
+        title: 'about'
+    },
+
+
+    {
+        path: "**",
+        title: "page not found",
+        component: PageNotFoundComponent
+    }
+];
